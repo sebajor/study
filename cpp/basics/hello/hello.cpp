c418a4c104abc533d423a91facc3a36114bdabfe
seba <seba@localhost.localdomain>
2023-11-01 20:00:22 -0300

    add cpp notes

---
+++ cpp/basics/hello/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout << "Hello world\n";
+}
+
+/*
+//first way.. here we use std::cout to tell the compiler that cout is in the std namespace
+int main(){
+    std::cout << "Hello world\n";
+}
+*/
+
